331ad9f016091e28aafdfa41fe82baeaaddef62c
zeff96 <zeffo1975@gmail.com>
2023-07-03 19:11:11 +0300

    Add transactione enity migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_03_090958) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_160943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_090958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "transaction_entries", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transaction_entries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_03_090958) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "transaction_entries", "users"
 end
