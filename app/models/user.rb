94ff2342e4282f183d9b0610a9c0d6cdbab8ada4
zeff96 <zeffo1975@gmail.com>
2023-07-03 12:14:30 +0300

    Add categories association and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         
+  
+  has_many :categories
+  
   validates :name, presence: true
 end
