34c72a64b573a2fafad45a19ae409e9f2abe35c8
zeff96 <zeffo1975@gmail.com>
2023-07-03 12:06:33 +0300

    Update routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root to: 'home#index', as: :unauthenticated_root
+    end
+  end
 end
