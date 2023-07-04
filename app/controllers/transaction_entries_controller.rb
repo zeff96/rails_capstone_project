84594812931780cd2acac710b90330c1b1938eef
zeff96 <zeffo1975@gmail.com>
2023-07-04 15:58:12 +0300

    Add show action for retrieving a given transaction_entry

--- app/controllers/transaction_entries_controller.rb
+++ app/controllers/transaction_entries_controller.rb
@@ -3,6 +3,10 @@ class TransactionEntriesController < ApplicationController
 		@transaction_entry = TransactionEntry.new
 	end
 
+	def show
+		@transaction_entry = TransactionEntry.find(params[:id])
+	end
+
 	def create
 		@transaction_entry = TransactionEntry.new(transaction_entry_params)
 		@transaction_entry.user = current_user
@@ -16,5 +20,11 @@ class TransactionEntriesController < ApplicationController
 				format.json {render json: @transaction_entry.errors, status: :unprocessable_entity}
 			end
 		end
+	end
+
+	private
+
+	def transaction_entry_params
+		params.require(:transaction_entry).permit(:name, :amount, :transaction_categories)
 	end
 end
