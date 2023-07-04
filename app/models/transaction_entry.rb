3bd6bf33af3bd322312404dd2a7dded835afb18f
zeff96 <zeffo1975@gmail.com>
2023-07-04 15:44:08 +0300

    Add transaction_entry validations

--- app/models/transaction_entry.rb
+++ app/models/transaction_entry.rb
@@ -2,4 +2,9 @@ class TransactionEntry < ApplicationRecord
   belongs_to :user
   has_many :transaction_categories
   has_many :categories, through: :transaction_categories
+
+  accept_nested_attributes_for :transaction_categories
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
